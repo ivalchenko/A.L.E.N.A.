c88d742f1faa7df74bc09c04d6a68170946c60cc
ivalchenko <i.valchenk@hotmail.com>
2015-04-28 06:20:25 +0300

    Added random method

---
+++ CSharp/Tools.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Tools
+    {
+        public static int RandomNumber(int end)
+        {
+            var rand = new Random();
+            return rand.Next(0, end);
+        }
+    }
+}
